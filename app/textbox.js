ab735a8dbccf30d67fe0fcd4c6c527e96fa7bafe
rakshith <62304358+rakshith-git@users.noreply.github.com>
2023-06-17 12:47:04 +0530

    smth

--- app/textbox.js
+++ app/textbox.js
@@ -152,13 +152,14 @@ function Textbox() {
                       </div>
                       <input
                         type="text"
-                        id="simple-search"
+                        id="editor"
                         onChange={(e) => {
                           setsearchTerm(e.target.value);
                         }}
                         onKeyDown={(e) => {
                           if (e.key === "Enter") {
                             handleSearch();
+                            console.log("lmfaoo")
                           }
                         }}
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
