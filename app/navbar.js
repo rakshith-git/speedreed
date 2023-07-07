0db86d47cd9ab10138993bfe52852a70942552bf
rakshith <62304358+rakshith-git@users.noreply.github.com>
2023-07-07 14:56:28 +0530

    reed haha

--- app/navbar.js
+++ app/navbar.js
@@ -44,7 +44,7 @@ function Navbar() {
           </svg>
           
           <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
-            RSVP
+            Reed
           </span>
           </Link>
           
