4d4b1367d6f3b03bf4692eeba6afcc3d7b7f9b5b
rakshith <62304358+rakshith-git@users.noreply.github.com>
2023-09-06 23:46:12 +0530

    Update firebaseConfig.js

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -9,7 +9,7 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "lol",
+  apiKey: "AIzaSyDjiaZ2YcOqs2DBQ5ocVT-5MO9_OYTS3nk",
   authDomain: "speedreed-database.firebaseapp.com",
   projectId: "speedreed-database",
   storageBucket: "speedreed-database.appspot.com",
