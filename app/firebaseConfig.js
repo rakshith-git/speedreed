6c29148fc9902fe7d25d9c4836448b4068348e83
rakshith <62304358+rakshith-git@users.noreply.github.com>
2023-10-08 23:56:47 +0530

    ENV ADDED

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -9,7 +9,7 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: String(process.env.FIREBASE_API_KEY),
+  apiKey: String(process.env.NEXT_PUBLIC_FIREBASE_API_KEY),
   authDomain: "speedreed-database.firebaseapp.com",
   projectId: "speedreed-database",
   storageBucket: "speedreed-database.appspot.com",
