316fb76f8edf7fca0419f32d193dca353665e391
rakshith <62304358+rakshith-git@users.noreply.github.com>
2024-01-09 17:39:48 +0530

    homepage changes

--- app/page.js
+++ app/page.js
@@ -84,16 +84,7 @@ export default function Home() {
 
   return (
     <>
-      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-          Don't Just Read it Reed it!
-        </h1>
-        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
-          SpeedReed lets you unlock the full potential of human Language
-          processing using various methods of speed and comprenhension enhancing
-          technology
-        </p>{" "}
-      </div>
+
       <div className="mt-4 dark:text-gray-100"></div>
       {render}
 
@@ -106,6 +97,16 @@ export default function Home() {
         >
           Finalize Text
         </Link>
+      </div>
+      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+          Don't Just Read it Reed it!
+        </h1>
+        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
+          SpeedReed lets you unlock the full potential of human Language
+          processing using various methods of speed and comprenhension enhancing
+          technology
+        </p>{" "}
       </div>
     </>
   );
